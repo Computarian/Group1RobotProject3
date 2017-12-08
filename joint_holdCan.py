d36d1fbda04e0d070d5ce1c468ea49c7f5e3ece6
Tim Jay <tim.joyce33@gmail.com>
2017-12-08 14:30:33 -0500

    added shoulder joint move file

---
+++ joint_holdCan.py
@@ -0,0 +1,45 @@
+# -*- encoding: UTF-8 -*-
+
+import sys
+from naoqi import ALProxy
+import time
+import almath
+import Robot_IP_Address
+
+def main(robotIP):
+    PORT = 9559
+
+    try:
+        motionProxy = ALProxy("ALMotion", robotIP, PORT)
+    except Exception,e:
+        print "Could not create proxy to ALMotion"
+        print "Error was: ",e
+        sys.exit(1)
+
+    #motionProxy.setStiffnesses("Head", 1.0)
+    motionProxy.setStiffnesses("RArm", 1.0)
+
+    # Simple command for the HeadYaw joint at 10% max speed
+    #names            = "HeadYaw"
+    names = "RShoulderPitch"
+    
+    angles           = 10*almath.TO_RAD
+    
+    fractionMaxSpeed = 0.1
+    motionProxy.setAngles(names,angles,fractionMaxSpeed)
+
+    
+    
+
+    time.sleep(3.0)
+    motionProxy.setStiffnesses("RArm", 0.0)
+
+if __name__ == "__main__":
+    robotIp = Robot_IP_Address.IP
+
+    if len(sys.argv) <= 1:
+        print "Usage python almotion_controllingjoints.py robotIP (optional default: 127.0.0.1)"
+    else:
+        robotIp = sys.argv[1]
+
+    main(robotIp)
